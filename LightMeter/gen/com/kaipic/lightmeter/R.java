52ead198db0f355918ac2cbf2dd53341b5334ef5
Kailuo Wang <kailuo.wang@gmail.com>
2010-08-03 18:06:37 -0400

    added help layout xmls

--- LightMeter/gen/com/kaipic/lightmeter/R.java
+++ LightMeter/gen/com/kaipic/lightmeter/R.java
@@ -21,21 +21,23 @@ public final class R {
         public static final int tag=0x7f020002;
     }
     public static final class id {
-        public static final int about=0x7f080009;
+        public static final int about=0x7f08000a;
         public static final int about_ok_button=0x7f080001;
-        public static final int apertureSpinner=0x7f080002;
-        public static final int calibrate=0x7f08000a;
-        public static final int exposureSpinner=0x7f080004;
-        public static final int exposureValue=0x7f080005;
-        public static final int isoSpinner=0x7f080003;
-        public static final int pause_button=0x7f080007;
-        public static final int shutterSpeed=0x7f080006;
-        public static final int status_text_view=0x7f080008;
+        public static final int apertureSpinner=0x7f080003;
+        public static final int calibrate=0x7f08000b;
+        public static final int exposureSpinner=0x7f080005;
+        public static final int exposureValue=0x7f080006;
+        public static final int help_ok_button=0x7f080002;
+        public static final int isoSpinner=0x7f080004;
+        public static final int pause_button=0x7f080008;
+        public static final int shutterSpeed=0x7f080007;
+        public static final int status_text_view=0x7f080009;
         public static final int textAboutVersion=0x7f080000;
     }
     public static final class layout {
         public static final int about_dialog=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int help_dialog=0x7f030001;
+        public static final int main=0x7f030002;
     }
     public static final class menu {
         public static final int option_menu=0x7f070000;
@@ -50,6 +52,9 @@ public final class R {
         public static final int copyright=0x7f050007;
         public static final int exposure_spinner_prompt=0x7f050006;
         public static final int hello=0x7f050000;
+        public static final int help_disclaimer=0x7f050010;
+        public static final int help_introduction=0x7f05000f;
+        public static final int help_usage=0x7f050011;
         public static final int iso_spinner_prompt=0x7f050005;
         public static final int ok=0x7f05000a;
         public static final int pause=0x7f050001;
